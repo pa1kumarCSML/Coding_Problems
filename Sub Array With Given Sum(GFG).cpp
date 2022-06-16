c0abac123acacb7ba616fcea5f9f612ac431da84
pavan kumar <64121659+pa1kumarCSML@users.noreply.github.com>
2022-06-16 12:10:43 +0530

    Subarray with given sum(GFG must do coding pbd)

---
+++ Sub Array With Given Sum(GFG).cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//     ***Subarray with given sum ***
+
+//link-->  https://practice.geeksforgeeks.org/problems/subarray-with-given-sum-1587115621/1#
+
+vector<int> subarraySum(int arr[], int n, long long s) // main function.
+    {
+        // Your code here
+        long long sum;
+        vector<int>sas;
+        for(int i=0;i<n;i++){
+            sum=s;
+            for(int j=i;j<n;j++){
+                sum-=arr[j];
+                if(sum<0)break;
+                if(sum==0){
+                        sas.push_back(i+1);
+                        sas.push_back(j+1);                  
+                    return sas;
+                    }
+                }
+            }
+        sas.push_back(-1);
+        return sas;
+        
+    }
+
+int main(){
+	// test input..
+	int arr[]={1, 2, 3, 7, 5};
+
+	vector<int>v=subarraySum(arr,5,12);
+    cout<<endl;
+	for(auto it: v)
+		cout<<it<<" ";
+}
+
+
+
+
